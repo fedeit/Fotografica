3dafcc576a56f083a437559f8ca985a06cd6853d
fedeit <fede.galbi@hotmail.com>
2020-10-24 15:04:22 +0200

    Implemented react component for the google map of all photos

---
+++ fotografica/src/components/PhotosGoogleMaps.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PhotoMap from './PhotoMap'
+import { getAllCoordinates } from '../api/fotografica_api'
+class PhotosGoogleMaps extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = { positions: [] }
+		getAllCoordinates((list) => {
+			this.setState({ positions: list })
+		})
+	}
+
+	render() {
+		return (
+			<PhotoMap markersType="cluster" positions={ this.state.positions }>
+		)
+	}
+}
